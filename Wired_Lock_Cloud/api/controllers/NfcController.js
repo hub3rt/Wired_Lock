6e829e4569fd3eac1d01f618cb7275bb4e60c9c2
hub3rt <hubert.girondiac@gmail.com>
2015-10-22 12:03:19 +0200

    Création base API
    Création des bases :
    
    - user
    - door
    - nfc

---
+++ Wired_Lock_Cloud/api/controllers/NfcController.js
@@ -0,0 +1,11 @@
+/**
+ * NfcController
+ *
+ * @description :: Server-side logic for managing nfcs
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
